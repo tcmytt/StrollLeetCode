14088a586e40e3df327729a55acd651fa25778b3
tcmytt <119166749+tcmytt@users.noreply.github.com>
2023-11-06 21:24:30 +0700

    Time: 7 ms (100.00%), Space: 15.1 MB (97.79%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,47 +1,59 @@
 class Solution {
 public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> wordCount;
-        for (const string& word : words) {
-            wordCount[word]++;
-        }
+    // The complexity of this function is O(nW).
+    double hash(double f, double code[], string &word) {
+        double result = 0.;
+        for (auto &c : word) result = result * f + code[c];
+        return result;
+    }
+    vector<int> findSubstring(string S, vector<string> &L) {
+        uniform_real_distribution<double> unif(0., 1.);
+        default_random_engine seed;
+        double code[128];
+        for (auto &d : code) d = unif(seed);
+        double f = unif(seed) / 5. + 0.8;
+        double value = 0;
 
-        vector<int> ans;
-        int n = s.length(), numWords = words.size(), wordLen = words[0].length(), totalLen = numWords * wordLen;
+        // The complexity of the following for loop is O(L.size( ) * nW).
+        for (auto &str : L) value += log(hash(f, code, str));
 
-        for (int i = 0; i < wordLen; ++i) {
-            int left = i, count = 0;
-            unordered_map<string, int> currentCount;
+        int unit = 1e9;
+        int key = (value-floor(value))*unit;
+        int nS = S.size(), nL = L.size(), nW = L[0].size();
+        double fn = pow(f, nW-1.);
+        vector<int> result;
+        if (nS < nW) return result;
+        vector<double> values(nS-nW+1);
+        string word(S.begin(), S.begin()+nW);
+        values[0] = hash(f, code, word);
 
-            for (int j = i; j <= n - wordLen; j += wordLen) {
-                string word = s.substr(j, wordLen);
-                
-                if (wordCount.find(word) != wordCount.end()) {
-                    currentCount[word]++;
-                    count++;
+        // Use a moving window to hash every word with length nW in S to a float number, 
+        // which is stored in vector values[]
+        // The complexity of this step is O(nS).
+        for (int i=1; i<=nS-nW; ++i) values[i] = (values[i-1] - code[S[i-1]]*fn)*f + code[S[i+nW-1]];
 
-                    while (currentCount[word] > wordCount[word]) {
-                        string leftWord = s.substr(left, wordLen);
-                        currentCount[leftWord]--;
-                        count--;
-                        left += wordLen;
-                    }
+        // This for loop will run nW times, each iteration has a complexity O(nS/nW)
+        // So the overall complexity is O(nW * (nS / nW)) = O(nS)
+        for (int i=0; i<nW; ++i) {
+            int j0=i, j1=i, k=0;
+            double sum = 0.;
 
-                    if (count == numWords) {
-                        ans.push_back(left);
-                        string leftWord = s.substr(left, wordLen);
-                        currentCount[leftWord]--;
-                        count--;
-                        left += wordLen;
-                    }
-                } else {
-                    currentCount.clear();
-                    count = 0;
-                    left = j + wordLen;
+            // Use a moving window to hash every L.size() continuous words with length nW in S.
+            // This while loop will terminate within nS/nW iterations since the increasement of j1 is nW,
+            // So the complexity of this while loop is O(nS / nW).
+            while(j1<=nS-nW) {
+                sum += log(values[j1]);
+                ++k;
+                j1 += nW;
+                if (k==nL) {
+                    int key1 = (sum-floor(sum)) * unit;
+                    if (key1==key) result.push_back(j0);
+                    sum -= log(values[j0]);
+                    --k;
+                    j0 += nW;
                 }
             }
         }
-
-        return ans;
+        return result;
     }
 };

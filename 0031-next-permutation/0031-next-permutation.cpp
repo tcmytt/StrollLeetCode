207eb24f40913da7ff1cf0a50ee8c9f8f94aed37
tcmytt <119166749+tcmytt@users.noreply.github.com>
2023-12-05 08:13:09 +0700

    Time: 2 ms (70.65%), Space: 12.4 MB (54.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-    
-    vector<int> DeQuy(vector<int> nums){
-        return {000};
-    }
-    
-    bool TangDanDeu(vector<int> arr,int n){
-        bool isIncreasing = true;
-        for (int i = 0; i < n-1; ++i) {
-            if (arr[i] > arr[i + 1]) {
-                isIncreasing = false;
-                break;
-            }
-        }
-        return isIncreasing;
-    }
-    
-    bool GiamDanDeu(vector<int> arr, int n){
-        bool isDecreasing = true;
-        for (int i = 0; i < n-1; ++i) {
-            if (arr[i] < arr[i + 1]) {
-                isDecreasing = false;
-                break;
-            }
-        }
-        return isDecreasing;        
-    }
-    
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return ;
-        
-        if(TangDanDeu(nums,n) == true) {
-            swap(nums[n-2],nums[n-1]);
-            return ;
-        }
-        
-        if(GiamDanDeu(nums,n) == true){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        
-        //
         int index = -1;
-        for(int i=n-2; i>=0; i--){
+        // 3 2 7 5 1
+        for(int i = n -2;i>=0;i--){
             if(nums[i] < nums[i+1]){
                 index = i;
                 break;
             }
         }
-        for(int i=n-1; i>=index && index != -1; i--){
-            if(nums[i] > nums[index]){
-                swap(nums[i], nums[index]);
+        
+        for(int i = n -1;i>=0 && index != -1 ;i--){
+            if(nums[i] > nums[index] ){
+                swap(nums[i],nums[index] );
                 break;
             }
         }
-        reverse(nums.begin() + index + 1, nums.end());
         
-
+        reverse(nums.begin()+index+1, nums.end());
+        
+        
     }
 };
